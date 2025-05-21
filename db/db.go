b092a841fe3a4004adbbe4e7cb3a046f6ee7282c
sameh-amr <samehamrsw@gmail.com>
2025-05-21 04:02:22 +0300

    added some fixes for the go files, and made sure that the db credentials are in the .env

--- db/db.go
+++ db/db.go
@@ -4,33 +4,39 @@ import (
     "database/sql"
     "fmt"
     "os"
+    "time"
 
     _ "github.com/lib/pq"
 )
 
 // ConnectDB opens a connection to PostgreSQL and returns *sql.DB
 func ConnectDB() (*sql.DB, error) {
-    host := os.Getenv("db")
-    port := os.Getenv("5432")
-    user := os.Getenv("postgres")
-    password := os.Getenv("secret")
-    dbname := os.Getenv("chatdb")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
 
     // Build the connection string
     psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
         host, port, user, password, dbname)
 
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-        return nil, err
+    var db *sql.DB
+    var err error
+
+    // Retry logic: try connecting up to 10 times with 2 seconds interval
+    for i := 0; i < 10; i++ {
+        db, err = sql.Open("postgres", psqlInfo)
+        if err == nil {
+            err = db.Ping()
+            if err == nil {
+                fmt.Println("Successfully connected to database!")
+                return db, nil
+            }
+        }
+        fmt.Printf("Attempt %d: Unable to connect to DB, retrying in 2 seconds...\n", i+1)
+        time.Sleep(2 * time.Second)
     }
 
-    // Test the connection
-    err = db.Ping()
-    if err != nil {
-        return nil, err
-    }
-
-    fmt.Println("Successfully connected to database!")
-    return db, nil
+    return nil, fmt.Errorf("could not connect to database after 10 attempts: %w", err)
 }
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "chatting-service-app/db" 
+)
 
 func main() {
-    fmt.Println("Hello from my Go project!")
+    // Connect to the database
+    database, err := db.ConnectDB()
+    if err != nil {
+        log.Fatal("Failed to connect to DB:", err)
+    }
+    defer database.Close()
+
+    // Just a simple HTTP handler for testing
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello from my Go project!")
+    })
+
+    fmt.Println("Server running on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
